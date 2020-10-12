f23c4a65ab07d189514a62fcb16693f9103f0a75
Davide Fauri <davide.fauri@gmail.com>
2020-10-12 13:56:51 +0200

    add Problems and Tests for Lists

--- problems/P_01_28_Lists.hs
+++ problems/P_01_28_Lists.hs
@@ -2,94 +2,99 @@
 
 module P_01_28_Lists where
 
-import Types
+-- import Types (NestedList) -- needed for problem 07
+-- import Types (Grouped) -- needed for problem 11
 
--- problem 01
+-- problem 01: Find the last element of a list.
 myLast = undefined
 
--- problem 02
+-- problem 02: Find the last but one element of a list.
 myButLast = undefined
 
--- problem 03
+-- problem 03: Find the K'th element of a list. The first element in the list is number 1.
 elementAt = undefined
 
--- problem 04
+-- problem 04: Find the number of elements of a list.
 myLength = undefined
 
--- problem 05
+-- problem 05: Reverse a list.
 reverse = undefined
 
--- problem 06
+-- problem 06: Find out whether a list is a palindrome.
 isPalindrome = undefined
 
--- problem 07
+-- problem 07: Flatten a nested list structure. Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
+-- you have to use the NestedList datatype
 flatten = undefined
 
--- problem 08
+-- problem 08: Eliminate consecutive duplicates of list elements. If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
 compress = undefined
 
--- problem 09
+-- problem 09: Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
 pack = undefined
 
--- problem 10
+-- problem 10: Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
 encode = undefined
 
--- problem 11
+-- problem 11: Modified run-length encoding. Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+-- you have to use the Grouped datatype
 encodeModified = undefined
 
--- problem 12
+-- problem 12: Decode a run-length encoded list. Given a run-length code list generated as specified in problem 11. Construct its uncompressed version.
+-- you have to use the Grouped datatype
 decodeModified = undefined
 
--- problem 13
+-- problem 13: Run-length encoding of a list (direct solution). Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem 9, but only count them. As in problem P11, simplify the result list by replacing the singleton lists (1 X) by X.
+-- you have to use the Grouped datatype
 encodeDirect = undefined
 
--- problem 14
+-- problem 14: Duplicate the elements of a list.
 dupli = undefined
 
--- problem 15
+-- problem 15: Replicate the elements of a list a given number of times.
 repli = undefined
 
--- problem 16
+-- problem 16: Drop every N'th element from a list.
 dropEvery = undefined
 
--- problem 17
+-- problem 17: Split a list into two parts; the length of the first part is given. Do not use any predefined predicates.
 split = undefined
 
--- problem 18
+-- problem 18: Extract a slice from a list. Given two indices, i and k, the slice is the list containing the elements between the i'th and k'th element of the original list (both limits included). Start counting the elements with 1.
 slice = undefined
 
--- problem 19
+-- problem 19: Rotate a list N places to the left. Hint: Use the predefined functions length and (++).
 rotate = undefined
 
--- problem 20
+-- problem 20: Remove the K'th element from a list.
 removeAt = undefined
 
--- problem 21
+-- problem 21: Insert an element at a given position into a list.
 insertAt = undefined
 
--- problem 22
+-- problem 22: Create a list containing all integers within a given range.
 range = undefined
 
--- problem 23
+-- problem 23: Extract a given number of randomly selected elements from a list.
 randomSelect = undefined
 
--- problem 24
+-- problem 24: Lotto: Draw N different random numbers from the set 1..M.
 diffSelect = undefined
 
--- problem 25
+-- problem 25: Generate a random permutation of the elements of a list.
 randomPermutation = undefined
 
--- problem 26
+-- problem 26: Generate the combinations of K distinct objects chosen from the N elements of a list
 combinations = undefined
 
--- problem 27a
+-- problem 27: Group the elements of a set into disjoint subsets. a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? Write a function that generates all the possibilities and returns them in a list.
 group = undefined
 
--- problem 27b
+-- problem 27: Group the elements of a set into disjoint subsets. b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a list of groups. Note that we do not want permutations of the group members; i.e. ((ALDO BEAT) ...) is the same solution as ((BEAT ALDO) ...). However, we make a difference between ((ALDO BEAT) (CARLA DAVID) ...) and ((CARLA DAVID) (ALDO BEAT) ...).
 groupSizes = undefined
 
--- problem 28a
+-- problem 28a: Sorting a list of lists according to length of sublists. a) We suppose that a list contains elements that are lists themselves. The objective is to sort the elements of this list according to their length. E.g. short lists first, longer lists later, or vice versa.
 lsort = undefined
 
--- problem 28b
+-- problem 28b: Sorting a list of lists according to length of sublists. b) Again, we suppose that a list contains elements that are lists themselves. But this time the objective is to sort the elements of this list according to their length frequency; i.e., in the default, where sorting is done ascendingly, lists with rare lengths are placed first, others with a more frequent length come later.
 lfsort = undefined
--- test/P_01_28_Lists_Tests.hs
+++ test/P_01_28_Lists_Tests.hs
@@ -3,16 +3,381 @@ module P_01_28_Lists_Tests (listsTests) where
 import qualified Problems as P
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.SmallCheck
+-- import Test.Tasty.QuickCheck
 import Types
 
-listsTests :: [TestTree]
+listsTests :: TestTree
 listsTests =
-  [ testGroup "Problem 01" problem01Tests
-  ]
+  testGroup
+    "Lists"
+    [ testGroup "Problem 01" problem01Tests,
+      testGroup "Problem 02" problem02Tests,
+      testGroup "Problem 03" problem03Tests,
+      testGroup "Problem 04" problem04Tests,
+      testGroup "Problem 05" problem05Tests,
+      testGroup "Problem 06" problem06Tests,
+      testGroup "Problem 07" problem07Tests,
+      testGroup "Problem 08" problem08Tests,
+      testGroup "Problem 09" problem09Tests,
+      testGroup "Problem 10" problem10Tests,
+      testGroup "Problem 11" problem11Tests,
+      testGroup "Problem 12" problem12Tests,
+      testGroup "Problem 13" problem13Tests,
+      testGroup "Problem 14" problem14Tests,
+      testGroup "Problem 15" problem15Tests,
+      testGroup "Problem 16" problem16Tests,
+      testGroup "Problem 17" problem17Tests,
+      testGroup "Problem 18" problem18Tests,
+      testGroup "Problem 19" problem19Tests,
+      testGroup "Problem 20" problem20Tests,
+      testGroup "Problem 21" problem21Tests,
+      testGroup "Problem 22" problem22Tests,
+      testGroup "Problem 23" problem23Tests,
+      testGroup "Problem 24" problem24Tests,
+      testGroup "Problem 25" problem25Tests,
+      testGroup "Problem 26" problem26Tests,
+      testGroup "Problem 27a" problem27aTests
+      -- testGroup "Problem 27b" problem27bTests,
+      -- testGroup "Problem 28a" problem28aTests,
+      -- testGroup "Problem 28b" problem28bTests
+    ]
 
 problem01Tests :: [TestTree]
 problem01Tests =
-  [ testCase "Finds the last element of a list of numbers" $ P.myLast [1, 2, 3, 4] @?= 4,
-    testCase "Finds the last element of a list of chars" $ P.myLast ['x', 'y', 'z'] @?= 'z'
+  [ testCase "Finds the last element of a list of numbers" $
+      P.myLast [1, 2, 3, 4]
+        @?= 4,
+    testCase "Finds the last element of a list of chars" $
+      P.myLast ['x', 'y', 'z']
+        @?= 'z'
+  ]
+
+problem02Tests :: [TestTree]
+problem02Tests =
+  [ testCase "Finds the last but one element of a list of numbers" $
+      P.myButLast [1, 2, 3, 4]
+        @?= 3,
+    testCase "Finds the last but one element of a list of chars" $
+      P.myButLast ['a' .. 'z']
+        @?= 'y'
+  ]
+
+problem03Tests :: [TestTree]
+problem03Tests =
+  [ testCase "Finds the 2nd element of a list of numbers" $
+      P.elementAt [1, 2, 3] 2
+        @?= 2,
+    testCase "Finds the 5th element of a string" $
+      P.elementAt "haskell" 5
+        @?= 'e'
+  ]
+
+problem04Tests :: [TestTree]
+problem04Tests =
+  [ testCase "Finds the number of elements of a list" $
+      P.myLength [123, 456, 789]
+        @?= 3,
+    testCase "Finds the number of characters of a string" $
+      P.myLength "Hello, world!"
+        @?= 13
+  ]
+
+problem05Tests :: [TestTree]
+problem05Tests =
+  [ testCase "Reverses a list of numbers" $
+      P.reverse [1, 2, 3, 4]
+        @?= [4, 3, 2, 1],
+    testCase "Reverses a string" $
+      P.reverse "A man, a plan, a canal, panama!"
+        @?= "!amanap ,lanac a ,nalp a ,nam A"
+  ]
+
+problem06Tests :: [TestTree]
+problem06Tests =
+  [ testCase "Checks that a list fails" $
+      P.isPalindrome [1, 2, 3]
+        @?= False,
+    testCase "Checks that a string succeeds" $
+      P.isPalindrome "madamimadam"
+        @?= True,
+    testCase "Checks that a list succeeds" $
+      P.isPalindrome [1, 2, 4, 8, 16, 8, 4, 2, 1]
+        @?= True
+  ]
+
+problem07Tests :: [TestTree]
+problem07Tests =
+  [ testCase "Flattens a list singleton" $
+      P.flatten (Elem 5)
+        @?= [5],
+    testCase "Flattens a nested list" $
+      P.flatten
+        (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+        @?= [1, 2, 3, 4, 5],
+    testCase "Flattens an empty list" $
+      P.flatten (List [])
+        @?= ([] :: [Int]) -- type annotation because ghc complains
+  ]
+
+problem08Tests :: [TestTree]
+problem08Tests =
+  [ testCase "Eliminates consecutive duplicates in a string" $
+      P.compress "aaaabccaadeeee"
+        @?= "abcade"
+  ]
+
+problem09Tests :: [TestTree]
+problem09Tests =
+  [ testCase "Packs consecutive chars into strings" $
+      P.pack
+        ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
+        @?= ["aaaa", "b", "cc", "aa", "d", "eeee"]
+  ]
+
+problem10Tests :: [TestTree]
+problem10Tests =
+  [ testCase "Compresses a string into run-length encoding" $
+      P.encode "aaaabccaadeeee"
+        @?= [(4, 'a'), (1, 'b'), (2, 'c'), (2, 'a'), (1, 'd'), (4, 'e')]
+  ]
+
+problem11Tests :: [TestTree]
+problem11Tests =
+  [ testCase "Compresses a string into run-length encoding, singletons aside" $
+      P.encodeModified "aaaabccaadeeee"
+        @?= [ Multiple 4 'a',
+              Single 'b',
+              Multiple 2 'c',
+              Multiple 2 'a',
+              Single 'd',
+              Multiple 4 'e'
+            ]
+  ]
+
+problem12Tests :: [TestTree]
+problem12Tests =
+  [ testCase "Decodes a run-length encoded list, with singletons" $
+      P.decodeModified
+        [ Multiple 4 'a',
+          Single 'b',
+          Multiple 2 'c',
+          Multiple 2 'a',
+          Single 'd',
+          Multiple 4 'e'
+        ]
+        @?= "aaaabccaadeeee"
+  ]
+
+--   context "When chaining encoding and decoding"
+--     $ prop "Is equivalent to identity"
+--     $ \x -> P.decodeModified . P.encodeModified $ x == x
+
+problem13Tests :: [TestTree]
+problem13Tests =
+  [ testCase "Implements direct run-length encoding without creating sublists" $
+      P.encodeDirect "aaaabccaadeeee"
+        @?= [ Multiple 4 'a',
+              Single 'b',
+              Multiple 2 'c',
+              Multiple 2 'a',
+              Single 'd',
+              Multiple 4 'e'
+            ]
+  ]
+
+--   context "When chaining direct encoding and decoding"
+--     $ prop "Is equivalent to identity"
+--     $ \x -> P.decodeModified . P.encodeDirect $ x == x
+
+problem14Tests :: [TestTree]
+problem14Tests =
+  [ testCase "Duplicates the elements of a list of numbers" $
+      P.dupli [1, 2, 3]
+        @?= [1, 1, 2, 2, 3, 3]
+  ]
+
+problem15Tests :: [TestTree]
+problem15Tests =
+  [ testCase "Replicates the elements of a string 3 times" $
+      P.repli "abc" 3
+        @?= "aaabbbccc"
+  ]
+
+problem16Tests :: [TestTree]
+problem16Tests =
+  [ testCase "Drops every 3rd element from a string" $
+      P.dropEvery "abcdefghik" 3
+        @?= "abdeghk"
+  ]
+
+problem17Tests :: [TestTree]
+problem17Tests =
+  [ testCase "Splits a list into two parts" $
+      P.split "abcdefghik" 3
+        @?= ("abc", "defghik")
+  ]
+
+problem18Tests :: [TestTree]
+problem18Tests =
+  [ testCase "Extracts a slice from a list" $
+      P.slice ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k'] 3 7
+        @?= "cdefg"
+  ]
+
+problem19Tests :: [TestTree]
+problem19Tests =
+  [ testCase "Rotates a list 3 places to the left" $
+      P.rotate ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] 3
+        @?= "defghabc",
+    testCase "Rotates a list -2 places to the left" $
+      P.rotate ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] (-2)
+        @?= "ghabcdef"
+  ]
+
+problem20Tests :: [TestTree]
+problem20Tests =
+  [ testCase "Removes the 2nd element from a string" $
+      P.removeAt 2 "abcd"
+        @?= ('b', "acd")
+  ]
+
+problem21Tests :: [TestTree]
+problem21Tests =
+  [ testCase "DESCRIBE ME" $
+      P.insertAt 'X' "abcd" 2
+        @?= "aXbcd"
+  ]
+
+problem22Tests :: [TestTree]
+problem22Tests =
+  [ testCase "DESCRIBE ME" $
+      P.range 4 9
+        @?= [4, 5, 6, 7, 8, 9]
+  ]
+
+problem23Tests :: [TestTree]
+problem23Tests =
+  [testCase "TODO" $ undefined]
+
+--   describe "problem23_randomSelect" $ do
+--     prop "Extracts N elements" $ \n xs -> length (P.randomSelect xs n) == n
+--     prop "Extracts elements from the given list"
+--       $ \n xs -> all (`elem` xs) (P.randomSelect xs n)
+
+problem24Tests :: [TestTree]
+problem24Tests =
+  [testCase "TODO" $ undefined]
+
+--   describe "problem24_diffSelect" $ do
+--     prop "Draws N different numbers"
+--       $ \n m -> length (nub (P.diffSelect n m)) == n
+--     prop "Draws numbers from the {1..M} set"
+--       $ \n m -> all (\x -> x >= 1 && x <= m) (P.diffSelect n m)
+
+problem25Tests :: [TestTree]
+problem25Tests =
+  [testCase "TODO" $ undefined]
+
+--   describe "problem25_randomPermutation" $ do
+--     context "When randomly permutating a string" $ do
+--       prop "Uses only elements from the string"
+--         $ \s -> all (`elem` s) (P.randomPermutation s)
+--       prop "Uses all elements from the string"
+--         $ \s -> length (P.randomPermutation s) == length s
+
+problem26Tests :: [TestTree]
+problem26Tests =
+  [ testCase "Produces the right initial elements of N choose K" $
+      take 3 (P.combinations 3 "abcdef")
+        @?= ["abc", "abd", "abe"]
+  ]
+
+problem27aTests :: [TestTree]
+problem27aTests =
+  [ testCase "Produces the right first disjoint subset" $
+      head
+        ( P.group
+            [ "aldo",
+              "beat",
+              "carla",
+              "david",
+              "evi",
+              "flip",
+              "gary",
+              "hugo",
+              "ida"
+            ]
+        )
+        @?= [ ["aldo", "beat"],
+              ["carla", "david", "evi"],
+              ["flip", "gary", "hugo", "ida"]
+            ],
+    testCase "Finds the correct number of disjoint subsets" $
+      (length :: [a] -> Int) -- type annotation because GHC complains
+        ( P.group
+            [ "aldo",
+              "beat",
+              "carla",
+              "david",
+              "evi",
+              "flip",
+              "gary",
+              "hugo",
+              "ida"
+            ]
+        )
+        @?= 1260
+  ]
+
+problem27bTests :: [TestTree]
+problem27bTests =
+  [ testCase "Produces the right first disjoint subset [2,2,5]" $
+      head
+        ( P.groupSizes
+            [2, 2, 5]
+            [ "aldo",
+              "beat",
+              "carla",
+              "david",
+              "evi",
+              "flip",
+              "gary",
+              "hugo",
+              "ida"
+            ]
+        )
+        @?= [ ["aldo", "beat"],
+              ["carla", "david"],
+              ["evi", "flip", "gary", "hugo", "ida"]
+            ],
+    testCase "Find the correct number of disjoint subsets [2,2,5]" $
+      (length :: [a] -> Int) -- type annotation because GHC complains
+        ( P.groupSizes
+            [2, 2, 5]
+            [ "aldo",
+              "beat",
+              "carla",
+              "david",
+              "evi",
+              "flip",
+              "gary",
+              "hugo",
+              "ida"
+            ]
+        )
+        @?= 756
+  ]
+
+problem28aTests :: [TestTree]
+problem28aTests =
+  [ testCase "Sorts a list based on length" $
+      P.lsort ["abc", "de", "fgh", "de", "ijkl", "mn", "o"]
+        @?= ["o", "de", "de", "mn", "abc", "fgh", "ijkl"]
+  ]
+
+problem28bTests :: [TestTree]
+problem28bTests =
+  [ testCase "Sorts a list based on length frequency" $
+      P.lfsort ["abc", "de", "fgh", "de", "ijkl", "mn", "o"]
+        @?= ["ijkl", "o", "abc", "fgh", "de", "de", "mn"]
   ]
