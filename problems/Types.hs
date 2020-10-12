1449257612b3379cd10f7f1e67a0056aacd29a02
Davide Fauri <davide.fauri@gmail.com>
2020-10-12 13:59:16 +0200

    add Types for Lists

--- problems/Types.hs
+++ problems/Types.hs
@@ -1,2 +1,7 @@
 module Types where
 
+-- problem 7
+data NestedList a = Elem a | List [NestedList a] deriving (Eq, Show)
+
+-- problem 11
+data Grouped a = Single a | Multiple Integer a deriving (Eq, Show)
